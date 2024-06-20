fc8f52b6ed21e966299d9bc56723f199c327b6c1
lee-donggyu <dlehdrb5509@naver.com>
2024-06-20 13:59:59 +0900

    feat: Add feature routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import {SignIn, Main, NotFoundPage, Projects, SignUp, Tasks} from './components/Components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/projects/:projectId' element={<Projects />}>
+              <Route path='tasks/:taskId' element={<Tasks />} />
+          </Route>
+          <Route path='*' element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
