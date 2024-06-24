2e9c56598a2fdf6776d6f4392031f28fbe3a37a9
Seong Min <ggbb0308@naver.com>
2024-06-24 09:22:21 +0900

    feat: task paging add

--- src/screens/task/index.js
+++ src/screens/task/index.js
@@ -14,49 +14,39 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useReducer, useRef } from "react";
 import { apiTask } from "../../axios/task";
 import TaskInfoModal from "../../components/task/info";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 const initialState = {
-  tasks: [
-    {
-      id: 1,
-      project_id: 1,
-      updated_at: new Date(),
-      created_at: new Date(),
-      due_date: new Date() + 2,
-      description: "description 1",
-      title: "title 1",
-      status: "PENDING",
-    },
-  ],
-  selectedIndex: 1,
+  tasks: [],
+  selectedIndex: null,
   modalOpen: false,
+  totalPage: 0,
+  currentPage: 0,
 };
 
-const reducer = (state = initialState, action) => {
-  // type - task : set, add, remove, update
-  // type - selectedIndex : set
-  // type - modal: open or close => toggle
+const reducer = (state, action) => {
   switch (action.type) {
     case "SET_TASKS":
-      return { ...state, tasks: action.payload };
+      console.log("작업 설정 중:", action.payload);
+      return { ...state, tasks: action.payload.tasks, totalPage: action.payload.totalPage };
     case "ADD_TASK":
       return { ...state, tasks: [...state.tasks, action.payload] };
     case "REMOVE_TASK":
-      return {
-        ...state,
-        tasks: state.tasks.filter((task) => task.id !== action.payload),
-      };
+      return { ...state, tasks: state.tasks.filter((task) => task.id !== action.payload) };
     case "UPDATE_TASK":
       return {
         ...state,
-        tasks: state.tasks.map((task) =>
-          task.id === action.payload.id ? action.payload : task,
-        ),
+        tasks: state.tasks.map((task) => (task.id === action.payload.id ? action.payload : task)),
       };
     case "SET_SELECTED_INDEX":
       return { ...state, selectedIndex: action.payload };
     case "TOGGLE_MODAL":
       return { ...state, modalOpen: !state.modalOpen };
+    case "SET_CURRENT_PAGE":
+      return { ...state, currentPage: action.payload };
+    default:
+      return state;
   }
 };
 
@@ -64,15 +54,30 @@ const TodoApp = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const navigate = useNavigate();
   const { projectId } = useParams();
+  const modalRef = useRef(null);
+
+  const fetchTasks = async (projectId, currentPage) => {
+    try {
+      const res = await apiTask.getTasks(projectId, currentPage);
+      console.log("API 응답:", res);
+
+      if (res.resultList && Array.isArray(res.resultList)) {
+        dispatch({
+          type: "SET_TASKS",
+          payload: { tasks: res.resultList, totalPage: res.totalPage },
+        });
+      } else {
+        console.error("resultList가 배열이 아니거나 정의되지 않음:", res.resultList);
+      }
+    } catch (error) {
+      console.error("작업을 가져오는 중 오류 발생:", error);
+    }
+  };
 
   useEffect(() => {
-    apiTask.getTasks(projectId).then((res) => {
-      console.log(res);
-      dispatch({ type: "SET_TASKS", payload: res });
-    });
-  }, [projectId]);
+    fetchTasks(projectId, state.currentPage);
+  }, [projectId, state.currentPage]);
 
-  // methods
   const toggleModal = (task) => () => {
     dispatch({ type: "TOGGLE_MODAL" });
     modalRef.current.setTask(task || {});
@@ -84,31 +89,34 @@ const TodoApp = () => {
   };
 
   const handleUpdateTask = (id, title, description, dueDate, status) => {
-    apiTask
-      .updateTask(projectId, id, title, description, dueDate, status)
-      .then((res) => {
-        dispatch({ type: "UPDATE_TASK", payload: res });
-      });
+    apiTask.updateTask(projectId, id, title, description, dueDate, status).then(() => {
+      fetchTasks(projectId, state.currentPage); // 작업 업데이트 후 다시 데이터를 가져옴
+    }).catch(error => {
+      console.error("작업을 업데이트하는 중 오류 발생:", error);
+    });
   };
 
   const handleInsertTask = (title, description, dueDate, status) => {
-    apiTask
-      .addTask(projectId, title, description, dueDate, status)
-      .then((res) => {
-        dispatch({ type: "ADD_TASK", payload: res });
-      });
+    apiTask.addTask(projectId, title, description, dueDate, status).then(() => {
+      fetchTasks(projectId, state.currentPage); // 작업 추가 후 다시 데이터를 가져옴
+    }).catch(error => {
+      console.error("작업을 추가하는 중 오류 발생:", error);
+    });
   };
 
   const handleDeleteTask = (id) => {
     apiTask.removeTask(projectId, id).then(() => {
-      console.log(id);
-      dispatch({ type: "REMOVE_TASK", payload: id });
+      fetchTasks(projectId, state.currentPage); // 작업 삭제 후 다시 데이터를 가져옴
+    }).catch(error => {
+      console.error("작업을 삭제하는 중 오류 발생:", error);
     });
   };
 
-  // refs
-  const modalRef = useRef(null);
-  console.log(state.tasks);
+  const handlePageChange = (event, value) => {
+    dispatch({ type: "SET_CURRENT_PAGE", payload: value - 1 });
+  };
+
+  console.log("현재 state.tasks:", state.tasks);
 
   return (
     <Grid container>
@@ -121,29 +129,30 @@ const TodoApp = () => {
         ref={modalRef}
         projectId={projectId}
       />
-      {/* Task List */}
       <Grid xs={7}>
         <TaskListBox>
           <TaskList>
-            {state.tasks.map((task) => (
-              <TaskItem status={task.status} container key={task.id}>
-                <Grid>
-                  {/*xs={2}*/}
-                  <TaskInfoIcon>
-                    <InfoOutlined onClick={toggleModal(task)} />
-                  </TaskInfoIcon>
-                </Grid>
-                <Grid>
-                  {/*xs={10}*/}
-                  <ListItemButton
-                    selected={state.tasks.includes(task.id)}
-                    onClick={handleListItemClick(task.id)}
-                  >
-                    <TaskName>{task.title}</TaskName>
-                  </ListItemButton>
-                </Grid>
-              </TaskItem>
-            ))}
+            {state.tasks.length > 0 ? (
+              state.tasks.map((task) => (
+                <TaskItem status={task.status} container key={task.id}>
+                  <Grid>
+                    <TaskInfoIcon>
+                      <InfoOutlined onClick={toggleModal(task)} />
+                    </TaskInfoIcon>
+                  </Grid>
+                  <Grid>
+                    <ListItemButton
+                      selected={state.selectedIndex === task.id}
+                      onClick={handleListItemClick(task.id)}
+                    >
+                      <TaskName>{task.title}</TaskName>
+                    </ListItemButton>
+                  </Grid>
+                </TaskItem>
+              ))
+            ) : (
+              <p>Task가 없습니다.</p>
+            )}
             <Divider />
             <ListItemButton onClick={toggleModal(null)}>
               <TaskAddIcon>
@@ -151,22 +160,20 @@ const TodoApp = () => {
               </TaskAddIcon>
             </ListItemButton>
           </TaskList>
+          <Stack spacing={2}>
+            <Pagination
+              count={state.totalPage}
+              page={state.currentPage + 1}
+              onChange={handlePageChange}
+            />
+          </Stack>
         </TaskListBox>
       </Grid>
       <Grid xs={5}>
-        {/* Steps */}
         <Outlet />
       </Grid>
     </Grid>
   );
-
-  // return (
-  //   <div>
-  //     {state.tasks.map((value) => (
-  //       <p>{value.title}</p>
-  //     ))}
-  //   </div>
-  // );
 };
 
 export default TodoApp;
